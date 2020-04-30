67a80306780c7ba1c3a259fe89d1a5be48cc4c39
operator.name <operator.name@protonmail.com>
2020-04-30 18:49:43 +0100

    Perspective wireframe

--- software-renderer.cpp
+++ software-renderer.cpp
@@ -4,6 +4,7 @@
 #include "practical.hpp"
 
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/transform.hpp>
 
 #include <glm/gtx/io.hpp>
@@ -21,9 +22,10 @@ void handleEvent(SDL_Event event);
 
 sdw::window window;
 
-float scale = 50;
-float x = 300;
-float y = 400;
+float scale = 1;
+float x = 0;
+float y = 0;
+float z = 0;
 
 // strictly for what is supported for rendering, whereas glmt::OBJ may include
 // more data that the renderer does not support
@@ -31,7 +33,26 @@ struct Model {
   typedef std::array<glmt::vec3l, 3> triangle_points;
   typedef std::tuple<triangle_points, glmt::rgbf01> triangle;
   std::vector<triangle> triangles;
+
   glm::mat4 matrix; // model matrix, TODO: proper types
+  glm::vec3 centre;
+};
+
+struct Camera {
+  glmt::vec3w target = glm::vec4(0, 0, 0, 1);
+
+  float dist = -3.2f;
+  float pitch = 0;
+  float yaw = 0.001;
+  float rot_velocity = 0.0002f;
+  float velocity = 0.003f;
+
+  glm::mat4 view() {
+    glm::vec4 pos = glm::vec4(0, 0, dist, 0) + target;
+    glm::mat4 rot = glm::rotate(yaw, glm::vec3(0, 1, 0)) *
+                    glm::rotate(pitch, glm::vec3(1, 0, 0));
+    return glm::translate(-glm::vec3(pos)) * rot;
+  }
 };
 
 struct State {
@@ -41,8 +62,10 @@ struct State {
   glmt::OBJ obj;
   Model model;
 
-  glm::mat4 view;
-  glm::mat4 proj;
+  glm::mat4 view = glm::lookAt(glm::vec3(0.1, 0.01, -10), glm::vec3(0, 0, 0),
+                               glm::vec3(0, 1, 0));
+  glm::mat4 proj = glm::perspectiveFov(glm::radians(90.f), (float)WIDTH,
+                                       (float)HEIGHT, 0.1f, 100.0f);
 
   struct SDL_detail {
     bool mouse_down = false;
@@ -69,25 +92,50 @@ int main(int argc, char *argv[]) {
 
 void setup() {
   state.obj = parse_obj("cornell-box.obj");
+
   state.model.triangles = state.obj.triangles;
+  glm::vec3 max(std::numeric_limits<float>::lowest());
+  glm::vec3 min(std::numeric_limits<float>::max());
+  for (auto const &t : state.model.triangles) {
+    for (glmt::vec3l point : std::get<0>(t)) {
+      max = glm::max(max, glm::vec3(point));
+      min = glm::min(min, glm::vec3(point));
+    }
+  }
+  state.model.centre = (min + max) / 2.f;
 
   // seed random state to be the same each time (for debugging)
   // TODO: add proper random state
   std::srand(0);
   window = sdw::window(WIDTH, HEIGHT, false);
-}
 
-void draw() {
-  window.clearPixels();
+  update();
   for (auto const &t : state.model.triangles) {
     std::array<glmt::vec2s, 3> ft;
     auto c = std::get<1>(t);
 
     for (size_t i = 0; i < ft.size(); i++) {
-      glmt::vec3w ws = state.model.matrix * glm::vec4(std::get<0>(t)[i]);
-      glmt::vec3c cs = state.proj * state.view * glm::vec4(ws);
+      glmt::vec3l ls = std::get<0>(t)[i];
+      std::cout << "ls:" << ls << std::endl;
+      glmt::vec3w ws = state.model.matrix * ls;
+      std::cout << "ws:" << ws << std::endl;
+      glmt::vec3c cs = state.proj * state.view * ws;
+      std::cout << "cs:" << cs << std::endl;
       // clip
-      ft[i] = glm::vec2(cs);
+
+      // glm::vec4 ss = cs;
+      // ss /= ss.w;
+      // ss *= glm::vec4(0.5, 0.5, 1, 1);
+      // ss += glm::vec4(0.5, 0.5, 0, 0);
+      // ss += glm::vec4(0, 0, 0, 0); // glm::vec4(viewport[0], viewport[1], 0,
+      // 0); ss *= glm::vec4(WIDTH, HEIGHT, 1,
+      //                 1); // glm::vec4(viewport[2], viewport[3], 1, 1);
+
+      glm::vec3 ss =
+          glm::project(glm::vec3(ls), state.view * state.model.matrix,
+                       state.proj, glm::vec4(0, 0, WIDTH, HEIGHT));
+
+      ft[i] = glm::vec2(ss);
     }
 
     // filledtriangle(window, std::make_tuple(ft, c));
@@ -95,13 +143,17 @@ void draw() {
   }
 }
 
+void draw() {
+  // window.clearPixels();
+}
+
 void update() {
   state.frame++;
 
   // Function for performing animation (shifting artifacts or moving the camera)
-  state.model.matrix = glm::translate(glm::vec3(x, y, 0)) *
-                       glm::scale(glm::vec3(scale, scale, scale)) *
-                       glm::rotate(glm::pi<float>(), glm::vec3(0, 0, 1));
+  state.model.matrix = glm::scale(glm::vec3(scale, scale, scale)) *
+                       glm::scale(glm::vec3(1, -1, 1)) *
+                       glm::translate(-state.model.centre);
 }
 
 void handleEvent(SDL_Event event) {
@@ -135,11 +187,15 @@ void handleEvent(SDL_Event event) {
       filledtriangle(window, state.filled_triangle);
       break;
     case SDLK_x:
-      std::cout << "scale = " << scale << ", x = " << x << ", y = " << y
-                << std::endl;
-      std::cin >> scale >> x >> y;
-      std::cout << "scale = " << scale << ", x = " << x << ", y = " << y
-                << std::endl;
+      std::cout << "scale\t"
+                << "x\t"
+                << "y\t"
+                << "z\t" << std::endl;
+      std::cin >> scale >> x >> y >> z;
+      std::cout << "scale\t"
+                << "x\t"
+                << "y\t"
+                << "z\t" << std::endl;
     }
     break;
   case SDL_MOUSEBUTTONDOWN:
